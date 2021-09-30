7196b576702a274dff7b7c6a35053a0259a75f63
ANDRES3021 <carlos.pardo@uptc.edu.co>
2021-09-30 15:26:03 -0700

    first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,10 @@
  */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(double));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(float));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(char));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(float));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(long int));
 	return (0);
 }
