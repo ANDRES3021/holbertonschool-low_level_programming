8f7254db1b54c10d10f46f7ea191b2fa75d20571
ANDRES3021 <carlos.pardo@uptc.edu.co>
2021-10-11 14:56:08 -0700

    first commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-*void reset_to98(int *n)  updates the value it pointer to 98
+* reset_to_98 - updates the value it pointer to 98
 *@n: pointer to n
-*Return: Always 0
+*
+*Return: void
 */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
