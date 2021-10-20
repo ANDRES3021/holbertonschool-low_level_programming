cb88515d3d2cb2352e29b9d4e397d6f9bdb622f4
ANDRES3021 <carlos.pardo@uptc.edu.co>
2021-10-19 19:11:55 -0700

    first commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _memcpy - copy memory areaa
+ *_memcpy - copy memory area
  *@dest: dest memory area
  *@src: source memory area
  *@n: bytes memory area to copy
  *
- *Return from area replaced
+ *Return: from area replaced
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
