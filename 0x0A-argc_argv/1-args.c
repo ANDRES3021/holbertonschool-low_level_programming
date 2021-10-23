85706efdcd4a6b6b04fb8adc6282c49306944392
ANDRES3021 <carlos.pardo@uptc.edu.co>
2021-10-22 17:09:45 -0700

    first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints the number of args
+ *@argc:argument count
+ *@argv: argument vector
+ *
+ * Return: Always zero
+ */
+int main(int argc, char __attribute__((unused))  *argv[])
+{
+	{
+		printf("%d\n", argc - 1);
+	}
+
+	return (0);
+}
