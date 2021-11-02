6d22381d3dca075af9fea7acd20bb043207e9e2d
ANDRES3021 <carlos.pardo@uptc.edu.co>
2021-11-01 21:01:42 -0700

    first commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,17 +3,14 @@
 #include "dog.h"
 
 /**
-  * free_dog - ...
-  * @d: ...
+  * free_dog - funtion that free dogs.
+  * @d: pointer
   *
-  * Return: ...
   */
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		free(d->owner);
-		free(d->name);
-		free(d);
+		free(d->owner),free(d->name),free(d);
 	}
 }
