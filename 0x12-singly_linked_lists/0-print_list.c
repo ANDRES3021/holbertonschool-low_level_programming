c54c8ddcf3ba5a5c441ce4fa9795a5078d207121
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-11-12 16:12:38 -0500

    good

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,16 +6,16 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t countnodos = 0;
+	size_t countnodos = 0;
 
-    while (h)
-    {
-        if (h->str == NULL)     
-            printf("[0] (nil)\n");
-            else 
-                printf("[%d], (%s\n)", h->len, h->str);
-                
-        countnodos++;
-        h= h->next;     
-    }
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		printf("[%d], (%s\n)", h->len, h->str);
+		countnodos++;
+		h = h->next;
+	}
+	return (countnodos);
 }
