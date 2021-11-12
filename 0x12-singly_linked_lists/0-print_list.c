e1be8ccf306bba9b1c599672e26036ccbe6d582b
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-11-12 16:20:29 -0500

    corrections

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,9 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		printf("[%d], (%s\n)", h->len, h->str);
+		{
+			printf("[%d], %s\n", h->len, h->str);
+		}
 		countnodos++;
 		h = h->next;
 	}
