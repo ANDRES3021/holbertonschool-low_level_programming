c9a94498e3d333d141282d9d4c787e9524024822
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-11-15 23:12:42 -0500

    again

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-void message(void) __attribute__((constructor));
-
 /**
  * message - Function that runs before main
  */
-void message(void)
+void __attribute__ ((constructor)) message (void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
