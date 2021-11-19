46830d1988d57ee8d0f20f39cf99c038d5be0cef
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-11-19 16:15:32 -0500

    add

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - check the code
+ * @n: int.
+ * @index: int
+ * Return: the value of the bit at index index
+ * or -1 if an error occured.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64 || !n)
+		return (-1);
+	*n = *n | (1 << index);
+	return (1);
+}
