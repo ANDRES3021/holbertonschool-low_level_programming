0c32770b53bed00773b353f94d4d123d36d63f5e
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-11-19 15:44:15 -0500

    index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - check the code
+ * get_bit - check the code
  * @n: int.
  * @index: int
  * Return: the value of the bit at index index
@@ -9,7 +9,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if(index > 64)
-	  return (-1);
+	if (index > 64)
+		return (-1);
 	return ((n >> index) & 1);
 }
