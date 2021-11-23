7cd1bd2ff8744b02672dd31e4a222f839cc20c6e
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-11-23 00:16:03 -0500

    better2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int create, count;
 
-	create = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	create = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (create == -1)
 	{
 		return (0);
