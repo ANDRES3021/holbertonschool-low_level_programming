0a440c145aaf8c4990d85c682c4244d56392f7cb
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-12-04 19:49:36 -0500

    arreglando betty

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,13 @@
  */
 int main(void)
 {
-int n = 48;
-while (n <= 57)
-{
-putchar(n);
-n += 1;
-}
-putchar('\n');
-return (0);
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n += 1;
+	}
+	putchar('\n');
+	return (0);
 }
