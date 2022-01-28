f65f0d0cb6fb627a31ebfd7f7735b82672506cda
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-01-28 10:47:32 -0500

    number 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Create the hash table.
+ * @size: Size of the table.
+ *
+ * Return: The hash table.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int index;
+	hash_table_t *hashtable;
+
+	if (size == 0)
+		return (NULL);
+	hashtable = malloc(sizeof(hash_table_t));
+	if (!hashtable)
+		return (NULL);
+	hashtable->size = size;
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+	if (hashtable->array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+	{
+		hashtable->array[index] = NULL;
+	}
+	return (hashtable);
+}
